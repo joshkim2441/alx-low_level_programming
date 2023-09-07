2b2a833c0ebad8369e9e31913013296b3b0ddb16
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-07 13:39:40 +0300

    Print a phrase followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - print the phrase "Programming is like building a multilingual puzzle"
+ * return: 0 if properly exited, non-zero if not
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
