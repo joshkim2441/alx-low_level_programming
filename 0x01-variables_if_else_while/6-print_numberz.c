5c04c4d291f28e67829fcc5fca3ed4cc30e2679c
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-08 21:49:07 +0300

    Print base 10 digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 single digits
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
