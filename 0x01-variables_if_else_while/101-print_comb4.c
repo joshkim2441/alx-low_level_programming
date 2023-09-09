887f31f5a57061489642c735b1efe6eb2c34f5b7
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-09 05:51:13 +0300

    Print all combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	int n, m, 1;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (1 = 50; 1 < 58; 1++)
+			for (l = 50; l < 58; l++)
 			{
-				if (n < m && m < 1)
+				if (n < m && m < l)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(1);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
