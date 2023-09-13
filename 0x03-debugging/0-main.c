e3a107f088dc015eeb92e0a911a439724a3aac0b
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-13 17:56:10 +0300

    "Test positive_or_negative function"

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * main - tests if the function positive_or_negative gives
- * the correct output in a case of 0
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
