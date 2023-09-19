99fca002131e81097dc6aabaf2da94b8d7d0e8e7
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-19 03:34:39 +0300

    "Convert a string to an integer"

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,42 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
+	int c, d, e, f, len, num;
 
-	while (*s)
+	c = 0;
+	d = 0;
+	e = 0;
+	f = 0;
+	len = 0;
+	num = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (c < len && f == 0)
 	{
-		if (*s == '-')
+		if (s[c] == '-')
+			++d;
+
+		if (s[c] >= '0' && s[c] <= '9')
 		{
-			sign *= -1;
-		} else if (*s >= '0' && *s <= '9')
-		{
-			result = result * 10 + (*s - '0');
-		} else if (result != 0 || sign != 1)
-		{
-			break;
+			num = s[c] - '0';
+
+			if (d % 2)
+				num = -num;
+			e = e * 10 + num;
+			f = 1;
+
+			if (s[c + 1] < '0' || s[c + 1] > '9')
+				break;
+
+			f = 0;
 		}
-		s++;
+		c++;
 	}
-	return (result * sign);
+	if (f == 0)
+		return (0);
+
+	return (e);
 }
 
