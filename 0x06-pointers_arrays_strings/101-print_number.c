f84a5fafa45481507b1ee423703f966c6af0aa9a
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-21 04:57:00 +0300

    "Print an integer"

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,20 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
+	} else
+	{
+		i = n;
 	}
-		if (n / 10)
+
+		if (i / 10)
 		{
-			print_number(n / 10);
+			print_number(i / 10);
 		}
-		_putchar(n % 10 + '0');
+		_putchar((i % 10) + '0');
 }
