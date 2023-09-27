da26ecaaf7b011683bd5dd0906bc0541bcfca47b
Joshua Mutuse <joshkm2441@gmail.com>
2023-09-27 14:42:49 +0300

    "Return 1 for a prime number"

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * prime - evaluates from 1 to n
+ * @x: integer to evaluate
+ * @y: iterates from 1 to n
+ * Return: 1 on success -1 for error
+ */
+
+int prime(int x, int y)
+{
+	if (y == 1)
+		return (1);
+	if (x % y == 0 && y > 0)
+		return (0);
+	return (prime(x, y - 1));
+}
+
+/**
  * is_prime_number - returns 1 if input
  * integer is a prime number
  * @n: input integer
@@ -9,16 +25,7 @@
 
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (prime(n, n - 1));
 }
