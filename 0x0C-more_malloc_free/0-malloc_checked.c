669bdfaab384adc1a40005171e8d3c7d48e40e5d
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-05 11:40:07 +0300

    "More malloc and free functions"

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -14,9 +14,7 @@ void *malloc_checked(unsigned int b)
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		exit(98);
-	}
+
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - checks if the characters are digits
+ * @s: the character to be checked
+ * Return: 0
+ */
+
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - checks if the number of arguments is correct
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
+	return (0);
+}
