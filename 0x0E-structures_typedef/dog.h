23b0664040f651689f8a8048e00c1e9e68f9b046
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-09 14:48:08 +0300

    "Task 1"

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,14 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - information about a dog
- * @name: pointer to first member
- * @age: floating point number
- * @owner: pointer to third member
- * Desription: a longer description.
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -17,9 +17,8 @@ struct dog
 };
 
 /**
- * dog_t - the typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -29,4 +28,4 @@ void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
-#endif /*STRTYP*/
+#endif
