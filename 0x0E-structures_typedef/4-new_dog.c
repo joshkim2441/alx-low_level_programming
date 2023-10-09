31cbafa79caebc2be636ba28171e7b4f366156ed
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-09 13:57:49 +0300

    "Task 4"

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,27 +12,44 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog = malloc(sizeof(dog_t));
+	int x, len_o, len_n;
 
-	if (newDog == NULL)
-		return (NULL);
+	struct dog *n_dog = NULL;
 
-	newDog->name = strdup(name);
-	if (newDog->name == NULL)
+	len_n = 0;
+	while (name[len_n] != '\0')
+		len_n++;
+
+	len_o = 0;
+	while (owner[len_o] != '\0')
+		len_o++;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog == NULL)
 	{
-		free(newDog);
+		free(n_dog);
 		return (NULL);
 	}
-
-	newDog->age = age;
-
-	newDog->owner = strdup(owner);
-	if (newDog->owner == NULL)
+	n_dog->name = malloc(len_n + 1);
+	if (n_dog->name == NULL)
 	{
-		free(newDog->name);
-		free(newDog);
+		free(n_dog->name);
+		free(n_dog);
 		return (NULL);
 	}
+	n_dog->owner = malloc(len_o + 1);
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	for (x = 0; x <= len_n; x++)
+		n_dog->name[x] = name[x];
+	for (x = 0; x <= len_o; x++)
+		n_dog->owner[x] = owner[x];
+	n_dog->age = age;
 
-	return (newDog);
+	return (n_Dog);
 }
