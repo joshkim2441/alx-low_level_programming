63bddce5e034c2a2236bccbac307674cee05cbc3
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-19 18:25:28 +0300

    "Task 5"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void premain(void)
 {
-	printf("Your're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("Your're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
