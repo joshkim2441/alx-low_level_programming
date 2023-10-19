77d951d2d998cb23e858975d0df560e0756a1f43
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-19 18:29:51 +0300

    "Task 5"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include "lists.h"
 
 /**
  * premain - is executed before main the main function
  */
 
-void premain(void)
+void __attribute__ ((constructor)) premain(void)
 {
 	printf("Your're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
