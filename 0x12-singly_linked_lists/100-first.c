00a0851340eeb66b7b389c2a8a029735c189af19
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-19 19:18:49 +0300

    "Task 5"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,5 @@
 
 void __attribute__ ((constructor)) first()
 {
-	printf("Your're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("Your're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
