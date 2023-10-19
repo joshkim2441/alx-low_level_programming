76f52d1c328179405746f1719932aa2b22e43f3c
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-19 18:18:20 +0300

    "Print before main"

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * premain - is executed before main the main function
  */
 
-void premain(void)
+void premain()
 {
 	printf("Your're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
