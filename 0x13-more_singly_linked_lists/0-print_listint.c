3b85cca8139425a6250dbcdb26ca964bc4392d61
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-25 05:37:35 +0300

    "Task 0"

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,14 +3,16 @@
 /**
  * print_listint - prints all elements of linked list
  * @h: linked list of listint_t type to be printed
- * 
- * Return: the number of nodes
+ *
+ * Return: the number of nodes, NULL if fail
 */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h)
 	{
 		printf("%d\n", h->n);
