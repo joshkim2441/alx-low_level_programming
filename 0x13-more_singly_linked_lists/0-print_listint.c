a101093ac9b341515321c255f74fe570df668b42
Joshua Mutuse <joshkm2441@gmail.com>
2023-10-25 05:48:10 +0300

    "Task 0"

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
+	int j;
 	size_t count = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h)
 	{
-		printf("%i\n", h->n);
+		j = h->n;
+		printf("%i\n", j);
 		h = h->next;
 		count++;
 	}
